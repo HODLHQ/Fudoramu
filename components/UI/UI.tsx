c2fbe61b7c2e087e577268122d1e4cb54973ae10
xCryptoBro <xcryptobro@gmail.com>
2022-04-19 09:06:40 -0700

    Favicon

--- components/UI/UI.tsx
+++ components/UI/UI.tsx
@@ -10,7 +10,24 @@ const UI = ({ children }: { children: ReactNode }) => {
       <Head>
         <title>Fudoramu</title>
         <meta name='description' content='Fudoramu' />
-        <link rel='icon' href='/favicon.gif' type='image/gif' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
       </Head>
       <Wallet />
       <Bar />
